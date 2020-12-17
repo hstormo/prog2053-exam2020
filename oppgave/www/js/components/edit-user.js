9f2c137a93ed907ab803e4ddd5dee4b13ecaf3a4
Håkon Stormo <hstormo@fastmail.com>
2020-12-17 10:37:57 +0100

    Render edit-user form

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -7,7 +7,32 @@ class EditUser extends LitElement {
     };
   }
 
-  // din kode her
+  render() {
+    if (this.user.uid)
+      return html`
+        <form action="api/updateUser.php" method="POST">
+          <input type="hidden" name="uid" value="${this.user.uid}">
+
+          <label for="firstName">First name</label>
+          <input type="text" id="firstName" name="firstName" value="${this.user.firstName}"><br>
+
+          <label for="lastName">Last name</label>
+          <input type="text" id="lastName" name="lastName" value="${this.user.lastName}"><br>
+
+          <label for="uname">Username</label>
+          <input type="text" id="uname" name="uname" value="${this.user.uname}"><br>
+
+          <label for="oldpwd">Current password</label>
+          <input type="password" id="oldpwd" name="oldpwd"><br>
+
+          <label for="pwd">New password</label>
+          <input type="password" id="pwd" name="pwd"><br>
+
+          <input type="submit" value="Submit"><br>
+        </form>
+      `;
+    else return "";
+  }
 
 }
 customElements.define('edit-user', EditUser);
